811bea0bb0e6884910756b432cb0ba9f4ecbce0b
Jorge Medina <medidrones@gmail.com>
2020-12-13 12:49:16 -0300

    Atualizacao de dependencias e codigo

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const Subscriber = require('../models/subscriber')
-const subscriber = require('../models/subscriber')
 
 router.get('/', async (req, res)=>{
     try {
